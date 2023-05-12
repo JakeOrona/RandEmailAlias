abae08f81f4a2d102fba8498bb4622cc37dda95e
BearBones <jakeorona@gmail.com>
2023-05-11 19:44:53 -0500

    Update setupMacOS.py

--- setupMacOS.py
+++ setupMacOS.py
@@ -1,7 +1,8 @@
+import os
 from setuptools import setup
 
 APP = ['RandEmailAlias.py']
-DATA_FILES = ['REAG-BG.jpg', 'REAM_icon.icns']
+DATA_FILES = [os.path.join(os.path.abspath('.'), 'REAG-BG.jpg'), os.path.join(os.path.abspath('.'), 'REAM_icon.icns')]
 OPTIONS = {
     'packages': ['random', 'string', 'smtplib', 'ssl', 'tkinter', 'pyperclip', 'threading'],
     'iconfile': 'REAM_icon.icns',
