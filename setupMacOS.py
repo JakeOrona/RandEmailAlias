88f1e1ffa9a2768db42412e7d9364a19c83648f9
BearBones <jakeorona@gmail.com>
2023-05-11 19:42:56 -0500

    Add files via upload

--- setupMacOS.py
+++ setupMacOS.py
@@ -1,18 +1,17 @@
 from setuptools import setup
 
 APP = ['RandEmailAlias.py']
-DATA_FILES = ["REAG-BG.jpg"]
+DATA_FILES = ['REAG-BG.jpg', 'REAM_icon.icns']
 OPTIONS = {
     'packages': ['random', 'string', 'smtplib', 'ssl', 'tkinter', 'pyperclip', 'threading'],
-    'iconfile': 'icon.icns',
+    'iconfile': 'REAM_icon.icns',
     'plist': {
         'CFBundleName': 'RandEmailAlias',
         'CFBundleShortVersionString': '2.4-beta',
         'CFBundleVersion': '2.4-beta',
         'CFBundleExecutable': 'RandEmailAlias',
         'LSUIElement': '1',
-    },
-    'resources': ['icon.icns'],
+    }
 }
 
 setup(
